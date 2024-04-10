a0f8519385e1a140b0ee2077ea49f651581f6df3
Johnny An <anhyeonju35@gmail.com>
2024-04-10 13:43:45 -0500

    Removed credentials.csv loading from load.sql

--- server/load.sql
+++ server/load.sql
@@ -26,16 +26,6 @@ IGNORE 1 LINES
 (ISBN, series_id, title, genre, copies, author, checked_out_by);
 
 
--- Load data for Credentials
-LOAD DATA INFILE './credentials.csv'
-INTO TABLE Credentials
-FIELDS TERMINATED BY ','
-ENCLOSED BY '"'
-LINES TERMINATED BY '\n'
-IGNORE 1 LINES
-(email, password);
-
-
 -- User generated data, no need for dummy data
 -- Load data for Reviews
 -- LOAD DATA INFILE './reviews.csv'
