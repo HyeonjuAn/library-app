771078b4bb60e0ffa825fb540d2ed91209d536f6
Anthony Tran <113156433+antomie@users.noreply.github.com>
2024-04-11 23:26:13 -0500

    whoops

--- client/src/components/BookCard.jsx
+++ client/src/components/BookCard.jsx
@@ -1,35 +1,16 @@
 // BookCard.jsx
-import React from "react";
+import React from 'react';
 
 const BookCard = ({ book }) => {
-  const goBack = () => {
-    window.history.back();
-  };
-
   return (
-    <div className=" flex flex-col justify-center items-center py-14 ">
-      <header className="text-gray-800 dark:text-white font-black text-3xl">
-        {book.title}:
-      </header>
-      <img src={pic} alt="{book.author}" className="w-45 h-auto mt-10 " />
-      <div className="pt-10 text-gray-800 dark:text-white font-black text-1xl">
-        {book.author}:
+    <div className="card">
+      <figure>
+        <img src={book.coverImage} alt={book.title} />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{book.title}</h2>
+        <p>{book.author}</p>
       </div>
-      <div className="pt-3 text-gray-800 dark:text-white font-black text-1xl">
-        {book.ibsn}:
-      </div>
-      <div className="pt-3 text-gray-800 dark:text-white font-black text-1xl">
-        {book.genre}:
-      </div>
-      <div className="pt-3 text-gray-800 dark:text-white font-black text-1xl">
-        {book.copies}:
-      </div>
-      <div className="pt-3 pb-10 text-gray-800 dark:text-white font-black text-1xl">
-        <p>Checked Out: {book.checked_out ? "Yes" : "No"}</p>
-      </div>
-      <button onClick={goBack} className="btn btn-outline btn-primary">
-        Go Back
-      </button>
     </div>
   );
 };
