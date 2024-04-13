114d680be0e2bfce02edc812ccbc35efbad66849
Anthony Tran <113156433+antomie@users.noreply.github.com>
2024-04-13 01:15:37 -0500

    added color for styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'lblue': '#424a59'
+            }
+        },
     },
     plugins: [require("daisyui")],
     daisyui: {
